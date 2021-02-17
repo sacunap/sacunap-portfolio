0b8e67bbebda8f0b4fdfae8d27f53d6bd53a8190
sacunap <sixto.acunap@gmail.com>
2021-02-17 11:38:03 -0300

    education

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,6 +1,15 @@
 import { Grid, Typography } from "@material-ui/core";
 import resumeData from "../../utils/resumeData";
+import CustomTimeline, {
+  CustomTimelineSeparator,
+} from "../../components/Timeline/index";
 import "./index.css";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import SchoolIcon from "@material-ui/icons/School";
 
 const Resume = () => {
   return (
@@ -11,11 +20,47 @@ const Resume = () => {
           <h6 className="section_title_text">About Me</h6>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant ="body2" className="aboutMe_text">{resumeData.about}</Typography>
+          <Typography variant="body2" className="aboutMe_text">
+            {resumeData.about}
+          </Typography>
         </Grid>
       </Grid>
 
-      <Grid container className="section"></Grid>
+      <Grid container className="section">
+        <Grid item className="section_title mb_30">
+          <span></span>
+          <h6 className="section_title_text">Resume</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container className="resume_timeline">
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Education History" icon={<SchoolIcon />}>
+                {resumeData.education.map((_education) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className="timeline_content">
+                      <Typography className="timeline_title">
+                        {_education.title}
+                      </Typography>
+                      <Typography variant="caption" className="timeline_date">
+                        {_education.date}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        className="timeline_description"
+                      >
+                        {_education.description}
+                      </Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+            {/* <Grid item sm={12} md={6}></Grid> */}
+          </Grid>
+        </Grid>
+      </Grid>
 
       <Grid container className="section"></Grid>
     </>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -23,5 +23,18 @@ export default {
   
   Profesional con gran apertura a nuevos aprendizajes, proactivo, motivado, innovador, orientado a resultados, motivación por el logro y trabajo en equipo con capacidad para integrar grupos profesionales multidisciplinarios en busca de una meta común. 
   
-  Con destacadas habilidades de comunicación efectiva, de rápida adaptación a diferentes tareas laborales y una clara disposición al cumplimiento de tareas.`
+  Con destacadas habilidades de comunicación efectiva, de rápida adaptación a diferentes tareas laborales y una clara disposición al cumplimiento de tareas.`,
+
+  education: [
+    {
+      title: "FullStack Web Developer - HENRY Bootcamp",
+      date: "Oct 2020 - Feb 2021",
+      description: "Bootcamp +700horas de programación basado en prácticas y proyectos reales."
+    },
+    {
+      title: "Ingeniero químico - USACH",
+      date: "2014 - 2019",
+      description: "Titulado de Ingeniería de ejecución en química de la Universidad de Santiago de Chile."
+    },
+  ]
 };
